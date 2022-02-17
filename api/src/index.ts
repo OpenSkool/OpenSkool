b2447ad205ceb7d74260d9d73136a708fd466dd0
Dieter Luypaert <dieterluypaert@gmail.com>
2022-02-17 11:33:04 +0100

    ✨ clean shutdown

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,4 +12,8 @@ const app = createApp({ logger: true })
 (async (): Promise<void> => {
   await app.ready();
   await app.listen(app.config.PORT);
+
+  const shutdown = () => void app.close();
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
 })().catch(app.log.error.bind(app.log));
