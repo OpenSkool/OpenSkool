5da5b9666f0daff3fa8a15730c49b669b1323fd8
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-03 21:40:09 +0100

    🔧 remove unused config

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    deps: { inline: ['nexus-prisma'] },
     setupFiles: ['test/setup.ts'],
     threads: false,
   },
