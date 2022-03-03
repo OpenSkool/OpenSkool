7c8c7142e5af460ab2276eecff98e9d80a435e02
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-03 21:25:39 +0100

    🌱 add competencies seed

---
+++ api/prisma/fixtures/competencies.ts
@@ -0,0 +1,210 @@
+export interface CompetencyFixture {
+  children?: CompetencyFixture[];
+  title: string;
+}
+
+export const competencyFixtures: CompetencyFixture[] = [
+  {
+    title: 'Medical Expert',
+    children: [
+      {
+        title:
+          'Practise medicine within their defined scope of practice and expertise',
+        children: [
+          {
+            title:
+              'Demonstrate a commitment to high-quality care of their patients',
+          },
+          {
+            title:
+              'Integrate the CanMEDS Intrinsic Roles into their practice of medicine',
+          },
+          {
+            title:
+              'Apply knowledge of the clinical and biomedical sciences relevant to their discipline',
+          },
+          {
+            title:
+              'Perform appropriately timed clinical assessments with recommendations that are presented in an organized manner',
+          },
+          {
+            title:
+              'Carry out professional duties in the face of multiple, competing demands',
+          },
+          {
+            title:
+              'Recognize and respond to the complexity, uncertainty, and ambiguity inherent in medical practice',
+          },
+        ],
+      },
+      {
+        title:
+          'Perform a patient-centred clinical assessment and establish a management plan',
+        children: [
+          {
+            title: 'Prioritize issues to be addressed in a patient encounter',
+          },
+          {
+            title:
+              'Elicit a history, perform a physical exam, select appropriate investigations, and interpret their results for the purpose of diagnosis and management, disease prevention, and health promotion',
+          },
+          {
+            title:
+              'Elicit a history, perform a physical exam, select appropriate investigations, and interpret their results for the purpose of diagnosis and management, disease prevention, and health promotion',
+          },
+          {
+            title:
+              'Establish goals of care in collaboration with patients and their families, which may include slowing disease progression, treating symptoms, achieving cure, improving function, and palliation',
+          },
+        ],
+      },
+      {
+        title:
+          'Plan and perform procedures and therapies for the purpose of assessment and/or management',
+        children: [
+          {
+            title: 'Determine the most appropriate procedures or therapies',
+          },
+          {
+            title:
+              'Obtain and document informed consent, explaining the risks and benefits of, and the rationale for, a proposed procedure or therapy',
+          },
+          {
+            title:
+              'Prioritize a procedure or therapy, taking into account clinical urgency and available resources',
+          },
+          {
+            title:
+              'Prioritize a procedure or therapy, taking into account clinical urgency and available resources',
+          },
+        ],
+      },
+      {
+        title:
+          'Establish plans for ongoing care and, when appropriate, timely consultation',
+        children: [
+          {
+            title:
+              'Implement a patient-centred care plan that supports ongoing care, follow-up on investigations, response to treatment, and further consultation',
+          },
+        ],
+      },
+      {
+        title:
+          'Actively contribute, as an individual and as a member of a team providing care, to the continuous improvement of health care quality and patient safety',
+        children: [
+          {
+            title:
+              'Recognize and respond to harm from health care delivery, including patient safety incidents',
+          },
+          {
+            title:
+              'Adopt strategies that promote patient safety and address human and system factors',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Communicator',
+    children: [
+      {
+        title:
+          'Establish professional therapeutic relationships with patients and their families',
+        children: [
+          {
+            title:
+              'Communicate using a patient-centred approach that encourages patient trust and autonomy and is characterized by empathy, respect, and compassion',
+          },
+          {
+            title:
+              'Optimize the physical environment for patient comfort, dignity, privacy, engagement, and safety',
+          },
+          {
+            title:
+              'Recognize when the values, biases, or perspectives of patients, physicians, or other health care professionals may have an impact on the quality of care, and modify the approach to the patient accordingly',
+          },
+          {
+            title:
+              'Recognize when the values, biases, or perspectives of patients, physicians, or other health care professionals may have an impact on the quality of care, and modify the approach to the patient accordingly',
+          },
+          {
+            title:
+              'Respond to a patient’s non-verbal behaviours to enhance communication',
+          },
+          {
+            title: 'Manage disagreements and emotionally charged conversations',
+          },
+        ],
+      },
+      {
+        title:
+          'Elicit and synthesize accurate and relevant information, incorporating the perspectives of patients and their families',
+        children: [
+          {
+            title:
+              'Use patient-centred interviewing skills to effectively gather relevant biomedical and psychosocial information',
+          },
+          {
+            title:
+              'Provide a clear structure for and manage the flow of an entire patient encounter',
+          },
+          {
+            title:
+              'Seek and synthesize relevant information from other sources, including the patient’s family, with the patient’s consent',
+          },
+        ],
+      },
+      {
+        title:
+          'Share health care information and plans with patients and their families',
+        children: [
+          {
+            title:
+              'Share information and explanations that are clear, accurate, and timely, while checking for patient and family understanding',
+          },
+          {
+            title:
+              'Disclose harmful patient safety incidents to patients and their families accurately and appropriately',
+          },
+        ],
+      },
+      {
+        title:
+          'Engage patients and their families in developing plans that reflect the patient’s health care needs and goals',
+        children: [
+          {
+            title:
+              'Facilitate discussions with patients and their families in a way that is respectful, non-judgmental, and culturally safe',
+          },
+          {
+            title:
+              'Assist patients and their families to identify, access, and make use of information and communication technologies to support their care and manage their health',
+          },
+          {
+            title:
+              'Use communication skills and strategies that help patients and their families make informed decisions regarding their health',
+          },
+        ],
+      },
+      {
+        title:
+          'Document and share written and electronic information about the medical encounter to optimize clinical decision-making, patient safety, confidentiality, and privacy',
+        children: [
+          {
+            title:
+              'Document clinical encounters in an accurate, complete, timely, and accessible manner, in compliance with regulatory and legal requirements',
+          },
+          {
+            title:
+              'Communicate effectively using a written health record, electronic medical record, or other digital technology',
+          },
+          {
+            title:
+              'Share information with patients and others in a manner that respects patient privacy and confidentiality and enhances understanding',
+          },
+        ],
+      },
+    ],
+  },
+];
---
+++ api/prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+
+import { CompetencyFixture, competencyFixtures } from './fixtures/competencies';
+
+const prisma = new PrismaClient();
+
+async function createCompetency(
+  fixture: CompetencyFixture,
+  nesting?: { root: string; parent: string },
+): Promise<void> {
+  const competency = await prisma.competency.create({
+    data: {
+      parentCompetencyId: nesting?.parent,
+      rootCompetencyId: nesting?.root,
+      translations: { create: { languageCode: 'EN', title: fixture.title } },
+    },
+  });
+  if (Array.isArray(fixture.children)) {
+    await createCompetencies(fixture.children, {
+      parent: competency.id,
+      root: nesting?.root ?? competency.id,
+    });
+  }
+}
+
+async function createCompetencies(
+  fixtures: CompetencyFixture[],
+  nesting?: { root: string; parent: string },
+): Promise<void> {
+  for (const fixture of fixtures) {
+    await createCompetency(fixture, nesting);
+  }
+}
+
+async function main(): Promise<void> {
+  await prisma.competency.deleteMany({ where: {} });
+  await createCompetencies(competencyFixtures);
+}
+
+(async (): Promise<void> => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    prisma.$disconnect();
+  }
+})();
