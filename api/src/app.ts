417dabd8e5dca30c62164ba966c8b6de03277b42
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-10 14:26:22 +0100

    ✨ add request logging toggle

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,7 +5,10 @@ import logger from './logger';
 import boomPlugin from './plugins/boom';
 import configPlugin from './plugins/config';
 
-const app = createApp({ logger });
+const app = createApp({
+  disableRequestLogging: process.env.DISABLE_REQUEST_LOGGING !== 'true',
+  logger,
+});
 
 app.register(configPlugin).register(boomPlugin).register(apiPlugin);
 
