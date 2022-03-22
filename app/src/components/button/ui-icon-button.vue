13a93ed8a1a719095641f3f9acc55e131070f4b7
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-22 16:43:07 +0100

    ✨ add icon button

---
+++ app/src/components/button/ui-icon-button.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  @apply inline-flex items-center p-2 rounded-lg text-amber-900 font-medium select-none;
+  @apply hover:bg-amber-200;
+  @apply focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-amber-500;
+}
+</style>
