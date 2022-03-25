5814180cf957677efc924e39cf3aa1d725106096
Iwan Lemmens <100413534+SiteSeerBE@users.noreply.github.com>
2022-03-25 17:21:07 +0100

    ✨ user select
    Box to temporarily select a user until real user-management is in place.
    
    Co-Authored-By: Dieter Luypaert <38758+Moeriki@users.noreply.github.com>

---
+++ app/src/components/user-select.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { GetPeopleQuery } from '~/generated/graphql';
+
+const { result } = useQuery<GetPeopleQuery>(gql`
+  query getPeople {
+    allPeople {
+      id
+      firstName
+      lastName
+    }
+  }
+`);
+
+const people = useResult(result, [], (data) => data.allPeople);
+
+const selectedPersonId = ref<string>();
+
+const selectedPerson = computed(() =>
+  people.value.find((item) => item.id === selectedPersonId.value),
+);
+</script>
+<template>
+  <ui-listbox
+    v-model="selectedPersonId"
+    :selected-label="
+      selectedPerson == null
+        ? 'Kies een gebruiker'
+        : selectedPerson?.firstName + ' ' + selectedPerson?.lastName
+    "
+  >
+    <ui-listbox-option
+      v-for="person in people"
+      :key="person.id"
+      :value="person.id"
+    >
+      {{ person.firstName + ' ' + person.lastName }}
+    </ui-listbox-option>
+  </ui-listbox>
+</template>
