b455badd10b30b9bfc5eb0853ab51166a6d305b2
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-25 11:35:21 +0100

    ✨ replace ui-button with button windi shortcut

--- app/themes/base.ts
+++ app/themes/base.ts
@@ -2,6 +2,13 @@ import type { Config } from 'windicss/types/interfaces';
 
 const base: Config = {
   attributify: true,
+  shortcuts: {
+    btn: 'inline-flex rounded-lg font-medium select-none px-10 py-2',
+    'btn-primary': `
+      bg-primary1-100 hover:bg-primary1-200 text-primary1-900
+      focus:outline-none focus-visible:(ring-2 ring-offset-2 ring-primary1-500)
+    `,
+  },
 };
 
 export default base;
