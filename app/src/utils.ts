94135e38936236f1a8d79c5d9c518cca671ea683
Iwan Lemmens <100413534+SiteSeerBE@users.noreply.github.com>
2022-03-28 15:36:36 +0200

    🐛 Random element from Array
    Math.round could introduce error when selecting random element from array.
    - Changed to Math.floor.

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -2,5 +2,5 @@ export function sample<T>(array: T[]): T {
   if (array.length === 0) {
     throw new Error('Can not sample empty array');
   }
-  return array[Math.round(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
