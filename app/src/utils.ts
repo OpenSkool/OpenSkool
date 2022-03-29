9a6f11328407d64519c6afc807e42d57374d9ce6
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-29 12:15:58 +0200

    ♻️ add and use assert util

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -1,3 +1,9 @@
+export function assert(value: unknown): asserts value {
+  if (value == null) {
+    throw new TypeError('expected value not to be nullish');
+  }
+}
+
 export function sample<T>(array: readonly T[]): T {
   if (array.length === 0) {
     throw new Error('Can not sample empty array');
