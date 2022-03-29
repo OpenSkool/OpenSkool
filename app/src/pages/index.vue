9e03e2c12aeded70a98c9aab364c3d2babc2f2dd
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-29 18:45:05 +0200

    🌐 add vue i18n

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,3 +1,15 @@
+<i18n lang="yaml">
+en:
+  message: Move along. Nothing to see here.
+</i18n>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+useI18n();
+</script>
+
 <template>
-  <p>Move along. Nothing to see here.</p>
+  <p v-t="'message'" />
+  <p v-t="'globalMessage'" />
 </template>
