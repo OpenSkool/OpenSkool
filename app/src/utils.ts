579626c44ba05e0c9a7e7da13692b3cfedd57e77
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-31 16:15:11 +0200

    ✨ store selected demo user in local storage
    Ref: #120

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -3,10 +3,3 @@ export function assert(value: unknown, label = 'value'): asserts value {
     throw new TypeError(`expected ${label} not to be nullish`);
   }
 }
-
-export function sample<T>(array: readonly T[]): T {
-  if (array.length === 0) {
-    throw new Error('Can not sample empty array');
-  }
-  return array[Math.floor(Math.random() * array.length)];
-}
