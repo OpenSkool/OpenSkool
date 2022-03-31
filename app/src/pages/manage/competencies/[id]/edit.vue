e939b2f7db001ff5c7c0a37a94c34710e534b173
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-31 13:28:15 +0200

    ⬆️ bump dev dependencies

--- app/src/pages/manage/competencies/[id]/edit.vue
+++ app/src/pages/manage/competencies/[id]/edit.vue
@@ -68,7 +68,6 @@ async function handleFormSubmit(): Promise<void> {
     formErrors.value.push('No active user id selected.');
     return;
   }
-  /* eslint-disable no-underscore-dangle */
   try {
     const response = await renameCompetency({
       currentUserId: demoStore.activeUserId,
