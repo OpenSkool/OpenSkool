41d9b2ed8ec96dd37b7f8dac83c9577ed2f26c61
Dieter Luypaert <dieterluypaert@gmail.com>
2022-03-31 14:03:15 +0200

    ✨ add NestedCompetency parent field
    Ref: #9

--- app/src/generated/graphql.ts
+++ app/src/generated/graphql.ts
@@ -117,6 +117,7 @@ export type NestedCompetency = Accountable & Competency & Node & {
   createdBy: Person;
   /** A CUID for a resource */
   id: Scalars['ID'];
+  parent: Competency;
   parentId: Scalars['ID'];
   subCompetencies?: Maybe<Array<NestedCompetency>>;
   title: Scalars['String'];
