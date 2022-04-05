87e38f90e87e069a11cb9d7f7fd6bad12fcacb37
Iwan Lemmens <100413534+SiteSeerBE@users.noreply.github.com>
2022-04-05 15:13:57 +0200

    🌐 add variables and language files

--- app/src/pages/manage/competencies/create.vue
+++ app/src/pages/manage/competencies/create.vue
@@ -1,5 +1,17 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+import { useI18nStore } from '~/i18n';
+const i18nStore = useI18nStore();
+i18nStore.loadGlob(import.meta.glob('~/locales/competencies.*.yaml'));
+
+const { t } = useI18n();
+</script>
 <template>
-  <ui-backbutton to="/manage/competencies">Competencies</ui-backbutton>
-  <h2 class="text-xl mb-3">Create competency</h2>
+  <ui-backbutton to="/manage/competencies">
+    {{ t('competencies.route.create.action.backButton') }}
+  </ui-backbutton>
+  <h2 class="text-xl mb-3">
+    {{ t('competencies.route.create.action.create') }}
+  </h2>
   <create-competency></create-competency>
 </template>
--- app/src/pages/manage/competencies/index.vue
+++ app/src/pages/manage/competencies/index.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import { GetAllRootCompetenciesDocument } from '~/generated/graphql';
 
+import { useI18n } from 'vue-i18n';
+import { useI18nStore } from '~/i18n';
+const i18nStore = useI18nStore();
+i18nStore.loadGlob(import.meta.glob('~/locales/competencies.*.yaml'));
+
+const { t } = useI18n();
+
 gql`
   query GetAllRootCompetencies {
     allRootCompetencies {
@@ -16,9 +23,9 @@ const competencies = useResult(result);
 </script>
 
 <template>
-  <h2 class="text-xl mb-3">Manage competencies</h2>
+  <h2 class="text-xl mb-3">{{ t('competencies.route.index.heading') }}</h2>
   <router-link class="btn btn-primary my-5" to="/manage/competencies/create">
-    Create competency
+    {{ t('competencies.route.index.action.create') }}
   </router-link>
   <ol class="list-decimal">
     <li v-for="competency of competencies" :key="competency.id">
