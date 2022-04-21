dabb769c39e851375b7f6376da5b1fb5d065a95f
Dieter Luypaert <dieterluypaert@gmail.com>
2022-04-21 10:37:07 +0200

    🔨 fix apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'default',
-      localSchemaFile: 'app/src/generated/schema.graphql',
+      localSchemaFile: 'api/src/generated/schema.graphql',
       url: 'http://localhost:3030/graphql',
     },
     includes: ['app/src/**/*.{gql,vue}'],
