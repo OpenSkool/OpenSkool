a263cc81bb97d635e02c43fe5a7c6d2afe7a9f21
Iwan Lemmens <github@siteseer.be>
2022-04-22 16:12:18 +0200

    ♻️ create reusable component for competency list Co-authored-by: shkurata

---
+++ app/src/components/list/competency-list.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+interface Competency {
+  id: string;
+  title: string;
+}
+
+defineProps<{
+  competencies: Competency[];
+  frameworkId: string;
+}>();
+</script>
+<template>
+  <ol class="list-decimal">
+    <li v-for="competency of competencies" :key="competency.id">
+      <router-link :to="`/manage/frameworks/${frameworkId}/${competency.id}`">
+        {{ competency.title }} {{ competency.id }}
+      </router-link>
+    </li>
+  </ol>
+</template>
