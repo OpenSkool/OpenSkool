cddb428ca088a8f40a010002fa3bf7913cd3f695
Dieter Luypaert <dieterluypaert@gmail.com>
2022-04-26 18:36:06 +0200

    ✅ increase timeouts on CI/CD

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,7 +1,10 @@
+import isCI from 'is-ci';
+import ms from 'ms';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    testTimeout: isCI ? ms('15s') : ms('5s'),
     setupFiles: ['test/setup.ts'],
     threads: false,
   },
