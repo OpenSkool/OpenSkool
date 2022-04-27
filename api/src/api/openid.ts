e7c4a9ab3bb5d5ff7ce518dea3973c72e47336e6
Dieter Luypaert <dieterluypaert@gmail.com>
2022-04-27 16:30:36 +0200

    ✨ retain current page on connect / logout
    Ref: #147

--- api/src/api/openid.ts
+++ api/src/api/openid.ts
@@ -1,3 +1,4 @@
+import { Static, Type } from '@sinclair/typebox';
 import { FastifyPluginAsync, onRequestAsyncHookHandler } from 'fastify';
 import { decodeJwt, JWTPayload } from 'jose';
 import { Issuer, generators, TokenSet } from 'openid-client';
@@ -9,6 +10,7 @@ declare module 'fastify' {
   interface Session {
     openId: {
       codeVerifier?: string;
+      state?: { from?: string };
       tokenSet?: TokenSet;
     };
   }
@@ -29,15 +31,25 @@ export const openIdRoutes: FastifyPluginAsync = async (app) => {
     response_types: ['code'],
   });
 
-  app.get('/connect', async (request, reply) => {
-    const codeVerifier = generators.codeVerifier();
-    request.session.openId.codeVerifier = codeVerifier;
-    const authUrl = client.authorizationUrl({
-      code_challenge: generators.codeChallenge(codeVerifier),
-      code_challenge_method: 'S256',
-      scope: 'openid email profile',
-    });
-    reply.redirect(authUrl);
+  const connectQuerystringSchema = Type.Object({
+    from: Type.Optional(Type.String()),
+  });
+
+  app.get<{
+    Querystring: Static<typeof connectQuerystringSchema>;
+  }>('/connect', {
+    async handler(request, reply) {
+      const codeVerifier = generators.codeVerifier();
+      request.session.openId.codeVerifier = codeVerifier;
+      request.session.openId.state = request.query;
+      const authUrl = client.authorizationUrl({
+        code_challenge: generators.codeChallenge(codeVerifier),
+        code_challenge_method: 'S256',
+        scope: 'openid email profile',
+      });
+      reply.redirect(authUrl);
+    },
+    schema: { querystring: connectQuerystringSchema },
   });
 
   app.get('/connect/callback', async (request, reply) => {
@@ -49,14 +61,17 @@ export const openIdRoutes: FastifyPluginAsync = async (app) => {
       reply.redirect(app.config.APP_BASE_URL);
       return;
     }
-    const { codeVerifier } = request.session.openId;
+    const { codeVerifier, state } = request.session.openId;
     request.session.openId.codeVerifier = undefined;
+    request.session.openId.state = undefined;
     try {
       const tokenSet = await client.callback(connectCallbackUrl, parameters, {
         code_verifier: codeVerifier,
       });
       request.session.openId.tokenSet = tokenSet;
-      reply.redirect(app.config.APP_BASE_URL);
+      const appUrl = new URL(app.config.APP_BASE_URL);
+      appUrl.pathname = state?.from ?? '/';
+      reply.redirect(appUrl.toString());
     } catch (error) {
       request.log.warn(error, 'openid callback could not verify token');
       reply.status(HTTP_STATUS_BAD_REQUEST);
@@ -64,24 +79,38 @@ export const openIdRoutes: FastifyPluginAsync = async (app) => {
     }
   });
 
-  app.get('/logout', async (request, reply) => {
-    if (request.session.openId.tokenSet == null) {
-      request.log.warn(`logout failed: no token set`);
-      reply.redirect(app.config.APP_BASE_URL);
-      return;
-    }
-    const { id_token: idToken } = request.session.openId.tokenSet;
-    await request.session.destroy();
-    reply.redirect(
-      client.endSessionUrl({
-        id_token_hint: idToken,
-        post_logout_redirect_uri: `${app.config.API_BASE_URL}${app.prefix}/logout/callback`,
-      }),
-    );
+  const logoutQuerystringSchema = Type.Object({
+    from: Type.Optional(Type.String()),
+  });
+
+  app.get<{
+    Querystring: Static<typeof logoutQuerystringSchema>;
+  }>('/logout', {
+    async handler(request, reply) {
+      if (request.session.openId.tokenSet == null) {
+        request.log.warn(`logout failed: no token set`);
+        reply.redirect(app.config.APP_BASE_URL);
+        return;
+      }
+      request.session.openId.state = request.query;
+      const { id_token: idToken } = request.session.openId.tokenSet;
+      request.session.openId.tokenSet = undefined;
+      reply.redirect(
+        client.endSessionUrl({
+          id_token_hint: idToken,
+          post_logout_redirect_uri: `${app.config.API_BASE_URL}${app.prefix}/logout/callback`,
+        }),
+      );
+    },
+    schema: { querystring: logoutQuerystringSchema },
   });
 
   app.get('/logout/callback', async (request, reply) => {
-    reply.redirect(app.config.APP_BASE_URL);
+    const appUrl = new URL(app.config.APP_BASE_URL);
+    const { state } = request.session.openId;
+    request.session.openId.state = undefined;
+    appUrl.pathname = state?.from ?? '/';
+    reply.redirect(appUrl.toString());
   });
 };
 
