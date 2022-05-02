54be0703d96988b721eea52d13e49a510cc66f37
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-02 19:04:11 +0200

    ⏪️ revert server codegen
    Ref: 59c26948b8ed7fe123be860c62ff004553fdd52e
    
    Not sure why I removed this. The schema should generate still each time
    the server starts up.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,18 @@
+import { exec } from 'child_process';
+
 import app from './app';
 
 (async (): Promise<void> => {
   await app.ready();
   await app.listen(app.config.PORT, app.config.HOST);
+
+  if (app.config.NODE_ENV === 'development') {
+    exec('yarn run codegen', (error) => {
+      if (error) {
+        app.log.error(error);
+      }
+    });
+  }
 
   const shutdown = () => void app.close();
   process.on('SIGINT', shutdown);
