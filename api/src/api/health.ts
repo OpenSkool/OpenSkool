463499e972dc4d5b2477c2ce081b04042843e66a
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-09 10:54:02 +0200

    🎨 skip the count check and return the check status in the response

--- api/src/api/health.ts
+++ api/src/api/health.ts
@@ -4,18 +4,12 @@ import plugin from 'fastify-plugin';
 import { prisma } from '../prisma';
 
 export const healthPlugin: FastifyPluginAsync = plugin(async (app) => {
-  const SERVER_ERROR = 500;
-  const HTTP_NO_CONTENT = 204;
-
   app.get('/health', async (request, reply) => {
     try {
-      const userCount = await prisma.user.count();
-      if (userCount === 0) {
-        reply.status(SERVER_ERROR);
-      }
-      reply.status(HTTP_NO_CONTENT);
+      await prisma.user.count();
+      reply.send({ database: 'ok' });
     } catch {
-      reply.status(SERVER_ERROR);
+      reply.send({ database: 'nok' });
     }
   });
 });
