30874080977be4aaa73a280b3a112840bf65c392
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-10 09:51:22 +0200

    💄 expose all windi neutrals

--- app/windicss/create-config.ts
+++ app/windicss/create-config.ts
@@ -1,9 +1,21 @@
+import windiColors from 'windicss/colors';
+import { DefaultColors } from 'windicss/types/config/colors';
 import { FullConfig } from 'windicss/types/interfaces';
 
 import formkit from './plugin-formkit';
 import { OsTheme } from './tokens';
 
-const BASE_COLORS = ['black', 'current', 'inherit', 'transparent', 'white'];
+const BASE_COLORS = [
+  // CSS
+  'current',
+  'inherit',
+  'transparent',
+  // B/W
+  'black',
+  'white',
+];
+
+const WINDI_COLORS = ['dark', 'light', 'neutral', 'slate', 'stone', 'zinc'];
 
 export default function createConfig(theme: OsTheme): FullConfig {
   return {
@@ -26,6 +38,13 @@ export default function createConfig(theme: OsTheme): FullConfig {
         ...theme.colors,
         ...BASE_COLORS.reduce(
           (base, color) => ({ ...base, [color]: color }),
+          {},
+        ),
+        ...WINDI_COLORS.reduce(
+          (base, color) => ({
+            ...base,
+            [color]: windiColors[color as keyof DefaultColors],
+          }),
           {},
         ),
       },
