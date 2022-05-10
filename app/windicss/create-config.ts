3808b2e2052e9daa4ac78041873bd302822997de
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-10 11:44:23 +0200

    🔥 remove unused shortcuts
    Ref: #189

--- app/windicss/create-config.ts
+++ app/windicss/create-config.ts
@@ -21,17 +21,6 @@ export default function createConfig(theme: OsTheme): FullConfig {
   return {
     attributify: true,
     plugins: [formkit],
-    shortcuts: {
-      btn: 'inline-flex rounded-lg font-medium text-base select-none px-10 py-2 cursor-pointer',
-      'btn-primary': `
-        bg-primary-300 hover:bg-primary-400 text-primary-600
-        focus:outline-none focus-visible:(ring-2 ring-offset-2 ring-primary-500)
-      `,
-      'btn-primary-outline': `
-        text-primary-600
-        focus:outline-none focus-visible:(ring-2 ring-offset-2 ring-primary-500)
-      `,
-    },
     theme: {
       ...theme,
       colors: {
