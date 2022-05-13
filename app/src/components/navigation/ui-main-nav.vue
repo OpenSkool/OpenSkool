d61072ef5c777bed8a3dff279c931ad11d8a9fdd
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-13 12:42:05 +0200

    💄  implement `hover` and `focus` states Ref: #217

--- app/src/components/navigation/ui-main-nav.vue
+++ app/src/components/navigation/ui-main-nav.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="my-5 text-base mr-5">
-    <ol class="space-y-2">
+  <nav class="my-5 mr-5 ml-2 min-w-max">
+    <ol class="space-y-1">
       <slot />
     </ol>
   </nav>
