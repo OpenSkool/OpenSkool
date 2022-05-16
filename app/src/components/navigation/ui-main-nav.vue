43f9128cfcb1ae7a6ae5cd6e6020142053b90e4b
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-16 12:31:46 +0200

    ✨  add the `OpenSkool` title in the navigation side bar

--- app/src/components/navigation/ui-main-nav.vue
+++ app/src/components/navigation/ui-main-nav.vue
@@ -1,7 +1,10 @@
 <template>
-  <nav
-    class="pt-5 my-5 mr-5 ml-2 min-w-max min-h-full border-3 border-gray-300"
-  >
+  <!-- <h3 v-t="'global.title'" class="text-4xl my-5 text-primary1-700" /> -->
+  <nav class="my-5 mr-5 ml-2 min-w-max min-h-full border-3 border-gray-300">
+    <UiTitle
+      v-t="'global.title'"
+      class="flex justify-center mb-5 text-xl border-b-3 border-gray-300"
+    />
     <ol class="space-y-1">
       <slot />
     </ol>
