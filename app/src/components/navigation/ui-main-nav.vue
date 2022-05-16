a1fb82d4f04ac15bab9b5e868d587b712aa725a1
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-16 11:43:39 +0200

    ✨  collapse the side navigation bar smoothly

--- app/src/components/navigation/ui-main-nav.vue
+++ app/src/components/navigation/ui-main-nav.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="pt-5 my-5 mr-5 ml-2 min-w-max border-3 border-gray-300">
+  <nav
+    class="pt-5 my-5 mr-5 ml-2 min-w-max min-h-full border-3 border-gray-300"
+  >
     <ol class="space-y-1">
       <slot />
     </ol>
