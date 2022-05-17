85dac5be956089beeee15284ab0b477d31e1fb84
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-17 17:41:33 +0200

    ♻️ extract loading spinner from layout

---
+++ app/src/domain/global/components/layout/loading-spinner.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { useGlobalQueryLoading } from '@vue/apollo-composable';
+
+const loading = useGlobalQueryLoading();
+</script>
+
+<template>
+  <TransitionRoot
+    :show="loading"
+    enter="duration-100 ease-out"
+    enter-from="opacity-0"
+    enter-to="opacity-100"
+    leave="delay-300 duration-100 ease-in"
+    leave-from="opacity-100"
+    leave-to="opacity-0"
+  >
+    <RiLoaderLine class="text-xl text-tertiary-300 spin" />
+  </TransitionRoot>
+</template>
