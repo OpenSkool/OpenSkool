dbb9ba4015a5cedcc764e1ffa9130e7875988b48
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-19 13:59:52 +0200

    💄  style the ordered list UI component
    Ref: #271

--- app/src/ui/list/ui-ordered-list-item.vue
+++ app/src/ui/list/ui-ordered-list-item.vue
@@ -9,22 +9,44 @@ defineEmits<(event: 'moveUp' | 'moveDown') => void>();
 </script>
 
 <template>
-  <li>
-    <slot />
-    <span v-if="showArrows">
-      <button class="arrow-up" @click="$emit('moveUp')">
+  <li
+    :class="[
+      'bg-white p-5 pl-12 list-inside relative text-sm',
+      'first-of-type:rounded-t-lg last-of-type:rounded-b-lg',
+    ]"
+  >
+    <div v-if="showArrows" class="absolute left-0 top-0 h-full">
+      <button class="arrow-up top-0 rounded-br-lg" @click="$emit('moveUp')">
         <span v-if="moveUpText" class="sr-only">{{ moveUpText }}</span>
         <RiArrowUpLine aria-hidden />
       </button>
-      <button class="arrow-down" @click="$emit('moveDown')">
+      <button
+        class="arrow-down bottom-0 rounded-tr-lg"
+        @click="$emit('moveDown')"
+      >
         <span v-if="moveDownText" class="sr-only">{{ moveDownText }}</span>
         <RiArrowDownLine aria-hidden />
       </button>
-    </span>
+    </div>
+    <slot />
   </li>
 </template>
 
 <style scoped>
+button {
+  @apply absolute p-1 pb-0 bg-gray-100/40;
+}
+
+li {
+  counter-increment: item;
+}
+
+li:before {
+  content: counter(item);
+  margin-right: 10px;
+  font-weight: bold;
+}
+
 li:first-of-type button.arrow-up {
   display: none;
 }
