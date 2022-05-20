a9ad8ccd9258ebc1a92f1852396b710e554c93bd
Nikolay Neykov <nikolai.neikov@gmail.com>
2022-05-20 13:38:00 +0200

    💄  move the `OpenSkool` logo in a `img` element and use `ol` for the sections

--- app/src/ui/main-nav/ui-main-nav-section.vue
+++ app/src/ui/main-nav/ui-main-nav-section.vue
@@ -5,9 +5,9 @@ defineProps<{
 </script>
 
 <template>
-  <section class="px-5 py-1">
+  <ul class="px-5 py-1">
     <UiMainNavHeader>{{ name }}</UiMainNavHeader>
     <slot />
     <div class="w-full border-b-1 border-gray-300 my-3" />
-  </section>
+  </ul>
 </template>
