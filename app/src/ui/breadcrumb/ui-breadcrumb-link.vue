a2c1948cae272dbcb15c47f67e6106e6dd645991
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-23 10:47:05 +0200

    💄 design tweaks
    - Uniform paddings / focus outline on interactive elements
    - Apply explicit text size everywhere
    - Simplify breadcrumb implementation

--- app/src/ui/breadcrumb/ui-breadcrumb-link.vue
+++ app/src/ui/breadcrumb/ui-breadcrumb-link.vue
@@ -1,11 +1,20 @@
 <script lang="ts" setup>
-defineProps<{ to: string }>();
+defineProps<{
+  to: string;
+}>();
 </script>
 
 <template>
-  <li class="uppercase text-gray-600 text-sm">
-    <RouterLink :to="to">
+  <li class="inline text-gray-600 text-sm uppercase">
+    <RouterLink class="hover:underline" :to="to">
       <slot />
     </RouterLink>
   </li>
 </template>
+
+<style scoped>
+li + li::before {
+  @apply mx-5 text-gray-400;
+  content: '/';
+}
+</style>
--- app/src/ui/breadcrumb/ui-breadcrumb.vue
+++ app/src/ui/breadcrumb/ui-breadcrumb.vue
@@ -1,20 +1,5 @@
 <template>
-  <ol class="flex flex-wrap breadcrumb">
+  <ol>
     <slot />
   </ol>
 </template>
-
-<style scoped>
-:global(.breadcrumb li + li::before) {
-  display: inline-block;
-  float: left;
-  content: ' ';
-  background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiBhcmlhLWhpZGRlbj0idHJ1ZSIgcm9sZT0iaW1nIiBjbGFzcz0iaWNvbmlmeSBpY29uaWZ5LS1tZGkiIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgcHJlc2VydmVBc3BlY3RSYXRpbz0ieE1pZFlNaWQgbWVldCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJjdXJyZW50Q29sb3IiIGQ9Im03IDIxbDcuOS0xOEgxN0w5LjEgMjFIN1oiPjwvcGF0aD48L3N2Zz4=');
-  background-repeat: none;
-  background-size: contain;
-  margin: 0.15em 1em;
-  width: 16px;
-  height: 16px;
-  font-size: 12pt;
-}
-</style>
