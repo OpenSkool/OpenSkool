7234f78c94933066b09ebc2a4dfd55a0f8f7ad4b
Dieter Luypaert <moeriki@hey.com>
2022-05-23 10:50:06 +0200

    🚨 lint api overrides correctly

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -6,12 +6,18 @@ module.exports = {
   },
   overrides: [
     {
-      files: 'src/',
+      files: 'src/**',
       extends: 'noise/node',
     },
     {
-      files: 'prisma/',
+      files: 'prisma/**',
       extends: ['noise/node', 'noise/node/script'],
+    },
+    {
+      files: ['*', '**/*.spec.*'],
+      rules: {
+        'node/no-unpublished-import': 'off',
+      },
     },
   ],
 };
