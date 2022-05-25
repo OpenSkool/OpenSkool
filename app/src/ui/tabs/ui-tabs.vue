abf8a5c16418d0891696d3d69341c94c10d309cf
Iwan Lemmens <github@siteseer.be>
2022-05-25 10:25:10 +0200

    💄 add button and style
    Ref: #211

--- app/src/ui/tabs/ui-tabs.vue
+++ app/src/ui/tabs/ui-tabs.vue
@@ -1,11 +1,25 @@
-<script lang="ts" setup></script>
-
 <template>
   <TabGroup>
-    <TabList v-if="$slots.default" class="bg-white flex gap-3">
-      <Tab v-for="slot in $slots.default()" :key="slot.props?.id">{{
-        slot.props?.title
-      }}</Tab>
+    <TabList
+      v-if="$slots.default"
+      id="tablist"
+      class="flex gap-6 p-2 overflow-x-auto"
+    >
+      <Tab
+        v-for="slot in $slots.default()"
+        v-slot="{ selected }"
+        :key="slot.props?.id"
+        as="template"
+      >
+        <button
+          class="border-b-3 inline-flex items-center font-semibold select-none text-base py-2 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 text-primary-600 uppercase whitespace-nowrap"
+          :class="[
+            selected ? ' border-b-secondary-300' : 'border-b-transparent',
+          ]"
+        >
+          {{ slot.props?.title }}
+        </button>
+      </Tab>
     </TabList>
     <TabPanels>
       <slot />
