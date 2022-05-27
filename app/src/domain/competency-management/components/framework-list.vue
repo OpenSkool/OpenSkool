784fc9c30c9e80f1a60ced9a1794693306fa8eed
Iwan Lemmens <github@siteseer.be>
2022-05-27 17:52:43 +0200

    ✨ handle empty frameworks list
    - Add error handling for frameworks
    - Add EmptyCard when no data is present
    Ref:#302

--- app/src/domain/competency-management/components/framework-list.vue
+++ app/src/domain/competency-management/components/framework-list.vue
@@ -10,19 +10,41 @@ gql`
   }
 `;
 
-const { result } = useQuery(GetAllCompetencyFrameworksDocument);
+const { error, loading, result } = useQuery(GetAllCompetencyFrameworksDocument);
 
 const frameworks = useResult(result);
+
+const ability = useAppAbility();
 </script>
 
 <template>
-  <UiOrderedList>
-    <UiOrderedListItem
-      v-for="framework of frameworks"
-      :key="framework.id"
-      :link-to="`/manage/frameworks/${framework.id}`"
-    >
-      {{ framework.title }}
-    </UiOrderedListItem>
-  </UiOrderedList>
+  <template v-if="error">
+    <p>Something went wrong</p>
+  </template>
+  <template v-else-if="loading">
+    <p>Loading</p>
+  </template>
+  <template v-else-if="frameworks == null">
+    <p>Not found</p>
+  </template>
+  <template v-else>
+    <UiOrderedList v-if="frameworks.length > 0">
+      <UiOrderedListItem
+        v-for="framework of frameworks"
+        :key="framework.id"
+        :link-to="`/manage/frameworks/${framework.id}`"
+      >
+        {{ framework.title }}
+      </UiOrderedListItem>
+    </UiOrderedList>
+    <EmptyCard v-else>
+      <p v-t="'frameworks.route.index.notFound'" />
+      <UiButtonRouterLink
+        v-if="ability.can('create', 'CompetencyFramework')"
+        v-t="'frameworks.route.index.action.create'"
+        class="my-5"
+        to="/manage/frameworks/create-framework"
+      />
+    </EmptyCard>
+  </template>
 </template>
