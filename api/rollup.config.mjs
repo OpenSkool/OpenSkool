49294e44b6cc9b843b090556325fc87eb8856a89
Dieter Luypaert <dieterluypaert@gmail.com>
2022-05-27 16:55:46 +0200

    🔨 switch to ESM

--- api/rollup.config.mjs
+++ api/rollup.config.mjs
@@ -13,11 +13,6 @@ export default [
     output: [
       {
         file: `dist/index.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `dist/index.mjs`,
         format: 'es',
         sourcemap: true,
       },
