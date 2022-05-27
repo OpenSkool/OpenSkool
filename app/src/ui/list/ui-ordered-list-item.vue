4906b3bb7585be136059ab9167abdd2dca10958f
Iwan Lemmens <github@siteseer.be>
2022-05-27 15:16:43 +0200

    ♿️ add hover & focus to lists
    Add hover & focus for link list, elements without link will not get hover nor focus.
    Ref: #296

--- app/src/ui/list/ui-ordered-list-item.vue
+++ app/src/ui/list/ui-ordered-list-item.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 defineProps<{
+  linkTo: string;
   moveUpText?: string;
   moveDownText?: string;
   showArrows?: boolean;
@@ -9,26 +10,34 @@ defineEmits<(event: 'moveUp' | 'moveDown') => void>();
 </script>
 
 <template>
-  <li
-    :class="[
-      'relative flex px-10 py-5 list-inside bg-white text-base',
-      'first-of-type:rounded-t-lg last-of-type:rounded-b-lg',
-    ]"
-  >
-    <div v-if="showArrows" class="absolute left-0 top-0 h-full">
-      <button class="arrow-up top-0 rounded-br-lg" @click="$emit('moveUp')">
-        <span v-if="moveUpText" class="sr-only">{{ moveUpText }}</span>
-        <RiArrowUpLine aria-hidden />
-      </button>
-      <button
-        class="arrow-down bottom-0 rounded-tr-lg"
-        @click="$emit('moveDown')"
-      >
-        <span v-if="moveDownText" class="sr-only">{{ moveDownText }}</span>
-        <RiArrowDownLine aria-hidden />
-      </button>
-    </div>
-    <slot />
+  <li>
+    <RouterLink
+      :to="linkTo"
+      :class="[
+        'relative flex px-10 py-5 list-inside bg-white text-base',
+        'first-of-type:rounded-t-lg last-of-type:rounded-b-lg',
+        'select-none focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
+        'hover:bg-gray-300/50',
+      ]"
+    >
+      <div v-if="showArrows" class="absolute left-0 top-0 h-full">
+        <button
+          class="arrow-up top-0 rounded-lg select-none focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 hover:bg-white"
+          @click.prevent="$emit('moveUp')"
+        >
+          <span v-if="moveUpText" class="sr-only">{{ moveUpText }}</span>
+          <RiArrowUpLine aria-hidden />
+        </button>
+        <button
+          class="arrow-down bottom-0 rounded-lg select-none focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 hover:bg-white"
+          @click.prevent="$emit('moveDown')"
+        >
+          <span v-if="moveDownText" class="sr-only">{{ moveDownText }}</span>
+          <RiArrowDownLine aria-hidden />
+        </button>
+      </div>
+      <slot />
+    </RouterLink>
   </li>
 </template>
 
@@ -40,11 +49,10 @@ button {
 li {
   counter-increment: item;
 }
-li:before {
+li a:before {
   content: counter(item);
   @apply font-bold mr-5;
 }
-
 li:first-of-type button.arrow-up {
   display: none;
 }
