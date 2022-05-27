bfb9e73cfba9238b2d46c533ac6ec0ec3e466a1b
Iwan Lemmens <github@siteseer.be>
2022-05-27 17:52:43 +0200

    ✨ handle empty frameworks list
    - Add error handling for frameworks
    - Add EmptyCard when no data is present
    Ref:#302

---
+++ app/src/ui/card/empty-card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div
+        class="bg-white flex justify-center gap-9 rounded-md text-center text-lg py-11"
+      >
+        <RiAlarmWarningLine />
+        <div><slot /></div>
+        <RiAlarmWarningLine />
+      </div>
+    </div>
+  </div>
+</template>
