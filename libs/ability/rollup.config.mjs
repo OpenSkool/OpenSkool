76f7bb15026e8e2e6e7c9aba97bded6babb0b460
Dieter Luypaert <hey@moeriki.com>
2022-05-30 09:51:10 +0200

    🔧 configure esbuild targets

--- libs/ability/rollup.config.mjs
+++ libs/ability/rollup.config.mjs
@@ -9,7 +9,20 @@ const bundle = (config) => ({
 
 export default [
   bundle({
-    plugins: [esbuild()],
+    plugins: [
+      // Lifted from Vite's default browserslist config
+      // > npx browserslist 'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0
+      esbuild({
+        target: [
+          'node16.15',
+          'chrome99',
+          'edge99',
+          'firefox91',
+          'ios12.2',
+          'safari14.1',
+        ],
+      }),
+    ],
     output: [
       {
         file: `dist/index.js`,
