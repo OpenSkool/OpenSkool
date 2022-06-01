030b16aff8b90b4d7ab56dab1b55c2444fdda375
Dieter Luypaert <hey@moeriki.com>
2022-06-01 14:18:43 +0200

    🔧 fix compile target
    - Postcss config was wrongly in root.
    
    - Vite auto detects that linked workspaces are part of the source code
      and will compile @os/ability correctly without all the config there.

--- libs/ability/rollup.config.mjs
+++ libs/ability/rollup.config.mjs
@@ -9,20 +9,7 @@ const bundle = (config) => ({
 
 export default [
   bundle({
-    plugins: [
-      // Lifted from Vite's default browserslist config
-      // > npx browserslist 'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0
-      esbuild({
-        target: [
-          'node16.15',
-          'chrome99',
-          'edge99',
-          'firefox91',
-          'ios12.2',
-          'safari14.1',
-        ],
-      }),
-    ],
+    plugins: [esbuild()],
     output: [
       {
         file: `dist/index.js`,
