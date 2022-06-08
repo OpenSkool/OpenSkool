122ced211713cc17ff6e0de85ac8568ccda95657
Dieter Luypaert <hey@moeriki.com>
2022-06-08 14:03:51 +0200

    🩹 use feature detection to skip matchMedia

--- app/src/hooks/use-media-query.ts
+++ app/src/hooks/use-media-query.ts
@@ -1,11 +1,15 @@
 import type { Ref } from 'vue';
 
 export function useMediaQuery(query: string): Ref<boolean> {
+  const matches = ref(true);
+  if (!('matchMedia' in window)) {
+    return matches;
+  }
   const mediaQuery = window.matchMedia(query);
-  const matches = ref(mediaQuery.matches);
   const update = (): void => {
     matches.value = mediaQuery.matches;
   };
+  update();
   onMounted(() => void mediaQuery.addEventListener('change', update));
   onBeforeUnmount(() => void mediaQuery.removeEventListener('change', update));
   return matches;
--- app/src/spec/render.ts
+++ app/src/spec/render.ts
@@ -1,6 +1,5 @@
 import { render as originalRender, RenderResult } from '@testing-library/vue';
 import { DefaultApolloClient } from '@vue/apollo-composable';
-import { vi } from 'vitest';
 import { createI18n } from 'vue-i18n';
 
 import { AppAbility, casl } from '~/ability';
@@ -11,19 +10,6 @@ import { router } from '~/router';
 
 export function render(component: any): RenderResult {
   const ability = new AppAbility([{ action: 'manage', subject: 'all' }]);
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: vi.fn().mockImplementation((query) => ({
-      matches: true,
-      media: query,
-      onchange: null,
-      addListener: vi.fn(), // deprecated
-      removeListener: vi.fn(), // deprecated
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      dispatchEvent: vi.fn(),
-    })),
-  });
   return originalRender(component, {
     global: {
       plugins: [
