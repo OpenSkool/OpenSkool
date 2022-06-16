0172777e047627af61532849c431cf72a5775e90
Dieter Luypaert <hey@moeriki.com>
2022-06-16 14:33:37 +0200

    ✨ expose prisma metrics in health

--- api/src/api/health.ts
+++ api/src/api/health.ts
@@ -1,15 +1,22 @@
+import { fetch } from 'cross-undici-fetch';
 import { FastifyPluginAsync } from 'fastify';
 import plugin from 'fastify-plugin';
 
 import { prisma } from '~/prisma';
 
+const HTTP_OK = 200;
+
 export const healthPlugin: FastifyPluginAsync = plugin(async (app) => {
   app.get('/health', async (request, reply) => {
-    try {
-      await prisma.user.count();
-      return reply.send({ database: 'ok' });
-    } catch {
-      return reply.send({ database: 'nok' });
-    }
+    const authApiResponse = await fetch(app.config.AUTH_ISSUER, {
+      method: 'HEAD',
+    });
+    const prismaMetrics = await prisma.$metrics.json();
+    return reply.send({
+      auth: {
+        isUp: authApiResponse.status === HTTP_OK,
+      },
+      prisma: prismaMetrics,
+    });
   });
 });
