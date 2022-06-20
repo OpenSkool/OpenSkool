8c8741de245899d65c4e073c41558fc24c6546a8
Dieter Luypaert <hey@moeriki.com>
2022-06-20 15:19:46 +0200

    🔧 move pino pretty config to code

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -4,6 +4,18 @@ export default pino({
   level: process.env.NODE_ENV === 'test' ? 'error' : 'info',
   transport:
     process.env.NODE_ENV === 'development'
-      ? { pipeline: [{ target: 'pino-pretty' }] }
+      ? {
+          pipeline: [
+            {
+              target: 'pino-pretty',
+              options: {
+                colorize: true,
+                ignore: 'hostname,pid',
+                levelFirst: true,
+                translateTime: 'HH:MM:ss',
+              },
+            },
+          ],
+        }
       : undefined,
 });
