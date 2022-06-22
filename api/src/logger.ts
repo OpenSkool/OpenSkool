03e555d49ff02fe42de7053573617ab904a075bf
Dieter Luypaert <hey@moeriki.com>
2022-06-22 10:50:29 +0200

    🩹 pipe to pino pretty
    As we did before. The new transport pipeline has a side-effect where
    it load dotenv-flow/config twice resulting in a bunch of warnings.

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -2,20 +2,4 @@ import pino from 'pino';
 
 export default pino({
   level: process.env.NODE_ENV === 'test' ? 'error' : 'info',
-  transport:
-    process.env.NODE_ENV === 'development'
-      ? {
-          pipeline: [
-            {
-              target: 'pino-pretty',
-              options: {
-                colorize: true,
-                ignore: 'hostname,pid',
-                levelFirst: true,
-                translateTime: 'HH:MM:ss',
-              },
-            },
-          ],
-        }
-      : undefined,
 });
