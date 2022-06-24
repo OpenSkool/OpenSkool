dd293f203b93ed573c1ec7a8e9f0bd15e331af13
Dieter Luypaert <hey@moeriki.com>
2022-06-24 12:39:18 +0200

    💄 update internship instance list design
    Ref: #262

--- app/src/pages/my-internships/[internshipInstanceId]/index.vue
+++ app/src/pages/my-internships/[internshipInstanceId]/index.vue
@@ -56,18 +56,27 @@ useHead(() => ({
 <template>
   <UiBreadcrumb>
     <UiBreadcrumbItem link-to="/manage/competencies">
-      {{ $t('global.mainMenu.internships') }}
+      {{ $t('global.mainMenu.internshipsHeading') }}
     </UiBreadcrumbItem>
   </UiBreadcrumb>
   <UiTitle is="h1" class="text-xl mb-3">My Internship</UiTitle>
-  <ul v-if="!loading" class="flex flex-wrap gap-5">
-    <li
-      v-for="position in internshipInstance?.availablePositions"
-      :key="position.id"
-      class="bg-gray-200 p-5 w-64"
-    >
-      <UiTitle is="h2">{{ position.organisation.name }}</UiTitle>
-      <p>{{ position.summary }}</p>
-    </li>
-  </ul>
+  <template v-if="!loading">
+    <ul class="grid gap-5">
+      <li
+        v-for="position in internshipInstance?.availablePositions"
+        :key="position.id"
+      >
+        <UiCard class="p-5">
+          <UiTitle is="h2">{{ position.organisation.name }}</UiTitle>
+          <p>{{ position.summary }}</p>
+        </UiCard>
+      </li>
+    </ul>
+  </template>
 </template>
+
+<style scoped>
+.grid {
+  grid-template-columns: repeat(auto-fill, minmax(30ch, 1fr));
+}
+</style>
