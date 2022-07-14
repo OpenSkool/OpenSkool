ac703a2b74b93e0c072a5945afdfb3040abecc03
Dieter Luypaert <moeriki@hey.com>
2022-07-14 16:00:52 +0200

    ⬆️ bump app dependencies

--- app/src/ui/button/ui-button-router-link.vue
+++ app/src/ui/button/ui-button-router-link.vue
@@ -1,17 +1,20 @@
 <script lang="ts" setup>
+import type { RouterLinkProps } from 'vue-router';
+
 import { createButtonStyles } from './helpers';
 
 const props = defineProps<{
   color?: 'primary' | 'danger';
   outline?: boolean;
   size?: 'sm' | 'base';
+  to: RouterLinkProps['to'];
 }>();
 
 const buttonStyles = computed(() => createButtonStyles(props));
 </script>
 
 <template>
-  <RouterLink :class="buttonStyles">
+  <RouterLink :class="buttonStyles" :to="to">
     <slot />
   </RouterLink>
 </template>
--- app/src/ui/main-nav/ui-main-nav-link.vue
+++ app/src/ui/main-nav/ui-main-nav-link.vue
@@ -1,7 +1,16 @@
+<script lang="ts" setup>
+import type { RouterLinkProps } from 'vue-router';
+
+defineProps<{
+  to: RouterLinkProps['to'];
+}>();
+</script>
+
 <template>
   <RouterLink
     active-class="font-medium bg-primary-200/50"
     class="rounded-md py-2 px-3 block hover:bg-gray-200 focus:outline-none focus-visible:(ring-2 ring-primary-700) "
+    :to="to"
   >
     <slot />
   </RouterLink>
