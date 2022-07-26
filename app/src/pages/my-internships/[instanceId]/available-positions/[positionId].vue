01ef85919d28781b0e5230aa890ac7931c3af8a6
Dieter Luypaert <hey@moeriki.com>
2022-07-26 17:17:19 +0200

    🚸 don't reload whole route on loading

--- app/src/pages/my-internships/[instanceId]/available-positions/[positionId].vue
+++ app/src/pages/my-internships/[instanceId]/available-positions/[positionId].vue
@@ -71,98 +71,92 @@ const internshipPosition = computed(() => result.value?.internshipPosition);
       }}
     </UiBreadcrumbItem>
   </UiBreadcrumb>
-  <template v-if="!loading">
-    <NotFoundLayout
-      v-if="internshipInstance == null || internshipPosition == null"
-    >
-      <p v-t="'internships.internshipPosition.error.notFound'" />
-    </NotFoundLayout>
-    <article v-else class="space-y-5">
-      <UiTitle is="h1" class="text-xl">
-        {{
-          $t('internships.internshipPosition.detail.heading', {
-            courseName: internshipInstance.internship.course.name,
-          })
-        }}
-      </UiTitle>
-      <UiCard>
-        <img
-          v-if="internshipPosition.organisation"
-          class="bg-light-500 organisation-image"
-          :src="internshipPosition.organisation.imageUrl"
-        />
-        <div class="p-5">
-          <UiSubtitle is="h2" class="mb-5">
-            {{ $t('internships.internshipPosition.section.summary.heading') }}
-          </UiSubtitle>
-          <p class="max-w-65ch">{{ internshipPosition.summary }}</p>
-        </div>
-      </UiCard>
-      <UiCard class="space-y-5 p-5">
-        <UiSubtitle is="h2" class="mb-5">Details</UiSubtitle>
-        <dl class="gap-5 md:grid">
-          <template v-if="internshipPosition.organisation">
-            <dt>
-              {{
-                $t(
-                  'internships.internshipPosition.section.details.term.organisation',
-                )
-              }}
-            </dt>
-            <dd>{{ internshipPosition.organisation.name }}</dd>
-          </template>
-          <template v-if="internshipPosition.workplace">
-            <dt>
-              {{
-                $t(
-                  'internships.internshipPosition.section.details.term.location',
-                )
-              }}
-            </dt>
-            <dd>{{ internshipPosition.workplace.plainAddress }}</dd>
-          </template>
+  <article
+    v-if="internshipInstance != null && internshipPosition != null"
+    class="space-y-5"
+  >
+    <UiTitle is="h1" class="text-xl">
+      {{
+        $t('internships.internshipPosition.detail.heading', {
+          courseName: internshipInstance.internship.course.name,
+        })
+      }}
+    </UiTitle>
+    <UiCard>
+      <img
+        v-if="internshipPosition.organisation"
+        class="bg-light-500 organisation-image"
+        :src="internshipPosition.organisation.imageUrl"
+      />
+      <div class="p-5">
+        <UiSubtitle is="h2" class="mb-5">
+          {{ $t('internships.internshipPosition.section.summary.heading') }}
+        </UiSubtitle>
+        <p class="max-w-65ch">{{ internshipPosition.summary }}</p>
+      </div>
+    </UiCard>
+    <UiCard class="space-y-5 p-5">
+      <UiSubtitle is="h2" class="mb-5">Details</UiSubtitle>
+      <dl class="gap-5 md:grid">
+        <template v-if="internshipPosition.organisation">
           <dt>
             {{
-              $t('internships.internshipPosition.section.details.term.period')
-            }}
-          </dt>
-          <dd>
-            {{
-              new Intl.DateTimeFormat($i18n.locale, {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric',
-              }).formatRange(
-                new Date(internshipInstance.internship.dateFrom),
-                new Date(internshipInstance.internship.dateTo),
+              $t(
+                'internships.internshipPosition.section.details.term.organisation',
               )
             }}
-          </dd>
-        </dl>
-      </UiCard>
-      <UiCard v-if="internshipPosition.mentors.length > 0" class="p-5">
-        <UiSubtitle is="h2" class="mb-5">
-          {{ $t('internships.internshipPosition.section.contact.heading') }}
-        </UiSubtitle>
-        <ul class="space-y-3">
-          <li
-            v-for="mentor of internshipPosition.mentors"
-            :key="mentor.id"
-            class="flex gap-5 items-center"
-          >
-            <img
-              class="rounded-full w-12 aspect-square"
-              :src="mentor.avatarUrl"
-            />
-            <div>
-              <h3 class="font-semibold">
-                {{ mentor.name }}
-              </h3>
-              <div v-t="'internships.roles.mentor'" />
-            </div>
-          </li>
-        </ul>
-      </UiCard>
+          </dt>
+          <dd>{{ internshipPosition.organisation.name }}</dd>
+        </template>
+        <template v-if="internshipPosition.workplace">
+          <dt>
+            {{
+              $t('internships.internshipPosition.section.details.term.location')
+            }}
+          </dt>
+          <dd>{{ internshipPosition.workplace.plainAddress }}</dd>
+        </template>
+        <dt>
+          {{ $t('internships.internshipPosition.section.details.term.period') }}
+        </dt>
+        <dd>
+          {{
+            new Intl.DateTimeFormat($i18n.locale, {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            }).formatRange(
+              new Date(internshipInstance.internship.dateFrom),
+              new Date(internshipInstance.internship.dateTo),
+            )
+          }}
+        </dd>
+      </dl>
+    </UiCard>
+    <UiCard v-if="internshipPosition.mentors.length > 0" class="p-5">
+      <UiSubtitle is="h2" class="mb-5">
+        {{ $t('internships.internshipPosition.section.contact.heading') }}
+      </UiSubtitle>
+      <ul class="space-y-3">
+        <li
+          v-for="mentor of internshipPosition.mentors"
+          :key="mentor.id"
+          class="flex gap-5 items-center"
+        >
+          <img
+            class="rounded-full w-12 aspect-square"
+            :src="mentor.avatarUrl"
+          />
+          <div>
+            <h3 class="font-semibold">
+              {{ mentor.name }}
+            </h3>
+            <div v-t="'internships.roles.mentor'" />
+          </div>
+        </li>
+      </ul>
+    </UiCard>
+    <template v-if="!loading">
       <UiAlert v-if="internshipInstance.appliedForPosition" color="info">
         <p>You have applied for this position.</p>
       </UiAlert>
@@ -172,8 +166,11 @@ const internshipPosition = computed(() => result.value?.internshipPosition);
         :position-id="positionId"
         @applied="refetch()"
       />
-    </article>
-  </template>
+    </template>
+  </article>
+  <NotFoundLayout v-else-if="!loading">
+    <p v-t="'internships.internshipPosition.error.notFound'" />
+  </NotFoundLayout>
 </template>
 
 <style scoped>
