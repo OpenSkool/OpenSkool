5c194d979c8eae6533f993d0db857f190aad76f0
Dieter Luypaert <hey@moeriki.com>
2022-09-05 10:45:29 +0200

    🔨 fix first-time codegen

--- api/scripts/codegen.ts
+++ api/scripts/codegen.ts
@@ -5,6 +5,7 @@ import prettier from 'prettier';
 
 import schema from '~/schema';
 
+await fs.mkdir(new URL('../src/codegen', import.meta.url), { recursive: true });
 await fs.writeFile(
   new URL('../src/codegen/schema.graphql', import.meta.url),
   prettier.format(printSchema(schema), { parser: 'graphql' }),
