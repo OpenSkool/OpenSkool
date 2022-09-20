05e79910ee647d0c6d690a9343d7c36f2af254f2
Dieter Luypaert <hey@moeriki.com>
2022-09-20 13:13:51 +0200

    💄 pretty log config

---
+++ api/pino-pretty.config.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+const { gray } = require('colorette');
+
+const LEVELS = {
+  10: '✏️', // trace
+  20: '🔎', // debug
+  30: '✨', // info
+  40: '⚠️ ', // warn
+  50: '‼️', // error
+  60: '💥', // fatal
+};
+
+module.exports = {
+  customPrettifiers: {
+    time: (value) => gray(value),
+    level: (value) => `${LEVELS[value]} `,
+  },
+  ignore: 'hostname,pid',
+};
