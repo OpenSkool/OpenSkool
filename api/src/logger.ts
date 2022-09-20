c0ca87e593d94968115778ee77cbb7de65ed8b0c
Dieter Luypaert <hey@moeriki.com>
2022-09-20 13:11:52 +0200

    🔥 remove unused prettifier

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -1,7 +1,5 @@
-import prettifier from '@mgcrea/pino-pretty-compact';
 import pino from 'pino';
 
 export default pino({
-  level: process.env.NODE_ENV === 'test' ? 'error' : 'info',
-  prettifier,
+  level: process.env.LOG_LEVEL ?? 'debug',
 });
