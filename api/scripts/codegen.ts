3cfafe14c8dbdd3eb5ad390f22b7b0a7ce8d74ab
Dieter Luypaert <hey@moeriki.com>
2022-10-26 12:01:11 +0200

    🎨 format codegen with correct prettier config

--- api/scripts/codegen.ts
+++ api/scripts/codegen.ts
@@ -1,3 +1,4 @@
+import assert from 'node:assert';
 import fs from 'node:fs/promises';
 
 import { printSchema } from 'graphql';
@@ -5,8 +6,14 @@ import prettier from 'prettier';
 
 import schema from '~/schema';
 
+const configFile = await prettier.resolveConfigFile();
+assert(configFile);
+const config = JSON.parse(
+	await fs.readFile(configFile, 'utf8'),
+) as prettier.Options;
+
 await fs.mkdir(new URL('../src/codegen', import.meta.url), { recursive: true });
 await fs.writeFile(
 	new URL('../src/codegen/schema.graphql', import.meta.url),
-	prettier.format(printSchema(schema), { parser: 'graphql' }),
+	prettier.format(printSchema(schema), { ...config, parser: 'graphql' }),
 );
